4b89c7309d5ad805c8c2f08e94aaef71402c05a1
Nadine <nadineatoccsaortiz@gmail.com>
2023-06-01 12:15:55 -0500

    Actualizacion de los ejercicios de  operadopres,asignacion,converciones,condicionales en python

--- Operadores/ejercicios.py
+++ Operadores/ejercicios.py
@@ -1,34 +1,32 @@
 #EJERCICIO 1
-# radiocirculo=int(input('ingrese el radio del circulo'))
-# area=3.14*(radiocirculo**2)
-# print(area)
-# print (f'el area el circulo es{area}')
-#print (f"""
-#===============================
-#el area el circulo es {area}
-#==============================
-#""")
+radiocirculo=int(input('ingrese el radio del circulo'))
+area=3.14*(radiocirculo**2)
+#print(area)
+#print (f'El area el circulo es{area}')
+print (f"""
+===============================
+El area el circulo es {area}
+==============================
+""")
 
 #EJERCICIO 2
-#radio=int(input('ingrese el radio de la esfera'))
-#volumen=4/3*3.14*(radio**3)
-#print (f"""
-#===============================
-#el volumen de la esfera es {volumen}
-#==============================
-#""")
+radio=int(input('ingrese el radio de la esfera'))
+volumen=4/3*3.14*(radio**3)
+print (f"""
+===============================
+El volumen de la esfera es {volumen}
+==============================
+""")
 
 #EJERCICIO 3
-#base=int(input('ingrese la base del trtiangulo'))
-#altura=int(input('ingrese la altura del triangulo'))
-#area=base*altura/2
-#print (f"""
-#===============================
-#el area el triangulo es {area}
-#==============================
-#""")
-
-
+base=int(input('ingrese la base del trtiangulo'))
+altura=int(input('ingrese la altura del triangulo'))
+area=base*altura/2
+print (f"""
+===============================
+El area el triangulo es {area}
+==============================
+""")
 
 
 #obtenercadena='hola'
@@ -51,55 +49,84 @@
 #print(con)
 
 #EJERCICIOS DE CONVERSIONES
-#nombre=input("Ingresa tu nombre")
-#apellido=input("Ingresa tu apellido")
-#print(f"""
-#===========================================
-#"{nombre}, {apellido}"
-#===========================================
-#""")
+nombre=input("Ingresa tu nombre")
+apellido=input("Ingresa tu apellido")
+print(f"""
+===========================================
+"Hola {nombre}, {apellido}
+Bienvenid@ al programa👾"
+===========================================
+""")
 
 #EJERCICIO 2
-#TEXTO=input("Ingrese un texto")
-#CantidadTexto=(len("TEXTO"))
-#print(CantidadTexto)
+TEXTO=input("Ingrese un texto")
+CantidadTexto=len(TEXTO)
+print(CantidadTexto)
 
 #EJRCICIO 3
-#Compararcion
-#comparacion=20
-#if comparacion!=40:
-#    print("afuera")
-#print("dentro")
+comparacion=int(input('Ingresa un numero para comparar'))
+if comparacion!=20:
+    print("afuera")
+else:
+    print("adentro")
 
 #EJERCICIO 4
-#edad=16
-#if edad<18:
-#    print("menor de edad")
-#if edad>18:
-#    print("mayor de edad")
+edad=int(input('ingresa tu edad'))
+if edad<18:
+    print("Eres menor de edad")
+if edad>18:
+    print("Eres mayor de edad")
 
 #TAREA 1:
-#DNI=input('Ingresa tu DNI')
-#LongDNI=(len(DNI))
-#if LongDNI==8:	
-#    nombre=input('ingresa tu nombre')	
-#    print(f"""	
-#    Bienvenid@, {nombre}	
-#    """)
-#else:	
-#    print('Error, vuelva a ingresar el DNI')
+DNI=input('Ingresa tu DNI')
+LongDNI=(len(DNI))
+if LongDNI==8:	
+    nombre=input('Ingresa tu nombre')	
+    print(f"""	
+    Bienvenid@, {nombre}	
+    """)
+else:	
+    print('Error, vuelva a ingresar el DNI')
 
 #TAREA 2:
-#apellido=input("Ingresa tu primer apellido")
-#comparacion=apellido[-2:]
-#if comparacion=='ez':
-#    print("Eres casi español")
-#if comparacion=='es':
-#    print("Eres casi peruano")
+apellido=input("Ingresa tu primer apellido")
+comparacion=apellido[-2:]
+if comparacion=='ez':
+    print("Eres casi español")
+if comparacion=='es':
+    print("Eres casi peruano")
 
-#TAREA 3:
-dni=input("ingresa tu dni")
-longdni=(len(dni))
-if longdni==8:
-    
-    print()
+#TAREA 3: SUMA DE DIGITOS
+DNI=input('Ingresa tu DNI')
+longitud=len(DNI)
+if longitud==8:
+	sumadigitos=int(DNI[-1:])+int(DNI[:1])
+	print(f"""
+	El primer digito de su DNI es:
+		{int(DNI[-1:])}
+	El ltimo digito de su DNI es:
+		{int(DNI[:1])}
+	La suma de ambos digitos es:
+    		{sumadigitos}
+	""")
+else:
+	print("Error, vuelva a ingresar su DNI")
+
+#TAREA 4:AÑO BICIESTO
+año =int(input('Ingresa el año que deseas evaluar'))
+if año % 4 == 0 and (año % 100 != 0 or año % 400 == 0):
+    print(f"""
+    Estamos evaluando su respuesta
+    ..............................
+    Respuesta:
+
+            --> El año que ingreso es bisiesto😎
+    """)
+else:
+	print(f"""
+    Estamos evaluando su respuesta
+    ..............................
+    Respuesta:
+
+            ---> El año que ingreso no es bisiesto🙁
+    """)
