69e057bdee91039b0503c150c8073aa9fd1dd207
Nadine <nadineatoccsaortiz@gmail.com>
2023-06-27 08:35:16 -0500

    controles de flujo

--- Controles_de_flujo/tartea for.py
+++ Controles_de_flujo/tartea for.py
@@ -1,6 +1,6 @@
 #TAREA, LIMITE 5
 animales=[]
 for numero in range(0,5):
-    datos=input('ingresa un  animal')
+    datos =input('ingresa un  animal: ')
     animales.append(datos)
-print(animales[numero])
+print(animales)
