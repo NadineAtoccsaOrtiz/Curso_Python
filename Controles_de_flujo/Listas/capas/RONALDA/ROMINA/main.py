cfc2df08cb5b07e2d8f0f78fd8a95772ff534fb7
Nadine <nadineatoccsaortiz@gmail.com>
2023-08-01 08:49:51 -0500

    actualizacion

---
+++ Controles_de_flujo/Listas/capas/RONALDA/ROMINA/main.py
@@ -0,0 +1,5 @@
+if contraseña == "1" or contraseña == "2" or contraseña == "3" or contraseña == "4":
+    print("¡Bienvenido!")
+else:
+    print("Contraseña incorrecta. Acceso denegado.")
+```
--- Controles_de_flujo/Listas/peliculas.py
+++ Controles_de_flujo/Listas/peliculas.py
@@ -1,9 +1,11 @@
 print("Categoria Precio Codigo Reacargo/dia de atraso")
-print("Favoritos 2.50 1 $0.50")
-print("Nuevos 3.00 2 $0.75")
-print("Estrenos 3.50 3 $1.00")
-print("Super estrenos 4.00 4 $1.50")
-print("")
+print(f"""
+1 Favoritos/ s/2.50 / s/0.50
+2 Nuevos/ s/3.00 2 / s/0.75
+3 Estrenos/ s/3.50 / s/1.00
+4 Super estrenos/ s/4.00 / s/1.50
+""")
+
 codigo = int(input("Introduce el codigo de la categoria de la pelicula: "))
 dias = int(input("Introduce el numero de dias de atraso en la devolucion: "))
 codigo in range(1,4)
@@ -13,13 +15,13 @@ if(codigo > 4):
 else:
     if(codigo==1):
         retraso = (2.50 + (dias * 0.5))
-        print("El total a pagar es: $",retraso," dolares")
+        print("El total a pagar es: s/",retraso," dolares")
     elif(codigo==2):
         retraso = (3 + (dias * 0.75))
-        print("El total a pagar es: $",retraso," dolares")
+        print("El total a pagar es: s/",retraso," dolares")
     elif(codigo==3):
         retraso = (3.50 + (dias))
-        print("El total a pagar es: $",retraso," dolares")
+        print("El total a pagar es: s/",retraso," dolares")
     else:
         retraso = (4 + (dias * 1.5))
-        print("El total a pagar es: $",retraso," dolares")
+        print("El total a pagar es: s/",retraso," dolares")
